d015220b3bd6449a313a0ce1967b0ee9df5bfec7
SeddonShen <283481855@qq.com>
2023-03-12 22:29:27 +0800

    Queue with multi signals

--- src/main/scala/gcd/QueueModule.scala
+++ src/main/scala/gcd/QueueModule.scala
@@ -1,10 +1,15 @@
 import chisel3._
 import chisel3.util._
 
+class StoreOrLoadInfo extends Bundle {
+  val addr = UInt(32.W)
+  val data = UInt(64.W)
+}
 class QueueModule extends Module{
   val io = IO (new Bundle{
-    val in  = Flipped(Decoupled(UInt(8.W)))
-    val out = Decoupled(UInt(8.W))
+    // val in  = Flipped(Decoupled(UInt(8.W)))
+    val in  = Flipped(Decoupled(new StoreOrLoadInfo()))
+    val out = Decoupled(new StoreOrLoadInfo())
   })
 
   val queue = Queue(io.in, 2)  // 2-element queue
--- src/test/scala/gcd/QueueModuleSpec.scala
+++ src/test/scala/gcd/QueueModuleSpec.scala
@@ -13,42 +13,42 @@ class QueueModuleSpec extends AnyFreeSpec with ChiselScalatestTester {
     test(new QueueModule()) { c =>
       c.io.out.ready.poke(false.B)	// 不能输出值
       c.io.in.valid.poke(true.B)  // 可以入列
-      c.io.in.bits.poke(42.U)	// 42准备入列
+      c.io.in.bits.poke((new StoreOrLoadInfo).Lit(_.addr -> 1.U, _.data -> 2.U))
       println(s"Starting:")
-      println(s"\tio.in: ready=${c.io.in.ready.peek().litValue}")
-      println(s"\tio.out: valid=${c.io.out.valid.peek().litValue}, bits=${c.io.out.bits.peek().litValue}")
+      println(s"\tio.in: ready=${c.io.in.ready.peek()}")
+      println(s"\tio.out: valid=${c.io.out.valid.peek()}, bits=${c.io.out.bits.peek()}")
       c.clock.step(1)	// 42入列
 
       c.io.in.valid.poke(true.B)  // 另一个元素可以入列
-      c.io.in.bits.poke(43.U)	// 43准备入列
+      c.io.in.bits.poke((new StoreOrLoadInfo).Lit(_.addr -> 3.U, _.data -> 4.U))	
       // 分析看看io.out.valid和io.out.bits会输出什么
       println(s"After first enqueue:")
-      println(s"\tio.in: ready=${c.io.in.ready.peek().litValue}")
-      println(s"\tio.out: valid=${c.io.out.valid.peek().litValue}, bits=${c.io.out.bits.peek().litValue}")
-      c.clock.step(1)	// 43入列
+      println(s"\tio.in: ready=${c.io.in.ready.peek()}")
+      println(s"\tio.out: valid=${c.io.out.valid.peek()}, bits=${c.io.out.bits.peek()}")
+      c.clock.step(1)	// (3,4)入列
 
       c.io.in.valid.poke(true.B)  // 准备入列，但是队列是满的
-      c.io.in.bits.poke(44.U)	// 给了信号但是入不了
+      c.io.in.bits.poke((new StoreOrLoadInfo).Lit(_.addr -> 5.U, _.data -> 6.U))	// 进不去了
       c.io.out.ready.poke(true.B)	// 可以出列了
       // What do you think io.in.ready will be, and will this enqueue succeed, and what will be read?
       println(s"On first read:")
       println(s"\tio.in: ready=${c.io.in.ready.peek()}")
       println(s"\tio.out: valid=${c.io.out.valid.peek()}, bits=${c.io.out.bits.peek()}")
-      c.clock.step(1)	// 42出列，44入列失败
+      c.clock.step(1)	// 第一次(1,2)出列，(5,6)入列失败
 
-      c.io.in.valid.poke(false.B)  // 不读元素了
-      c.io.out.ready.poke(true.B)	// 可以继续输出
-      // What do you think will be read here?
-      println(s"On second read:")
-      println(s"\tio.in: ready=${c.io.in.ready.peek()}")
-      println(s"\tio.out: valid=${c.io.out.valid.peek()}, bits=${c.io.out.bits.peek()}")
-      c.clock.step(1)	// 43出列，队列空了
+      // c.io.in.valid.poke(false.B)  // 不读元素了
+      // c.io.out.ready.poke(true.B)	// 可以继续输出
+      // // What do you think will be read here?
+      // println(s"On second read:")
+      // println(s"\tio.in: ready=${c.io.in.ready.peek()}")
+      // println(s"\tio.out: valid=${c.io.out.valid.peek()}, bits=${c.io.out.bits.peek()}")
+      // c.clock.step(1)	// 43出列，队列空了
 
-      // 这时候还会读到东西吗？
-      println(s"On third read:")
-      println(s"\tio.in: ready=${c.io.in.ready.peek()}")
-      println(s"\tio.out: valid=${c.io.out.valid.peek()}, bits=${c.io.out.bits.peek()}")
-      c.clock.step(1)
+      // // 这时候还会读到东西吗？
+      // println(s"On third read:")
+      // println(s"\tio.in: ready=${c.io.in.ready.peek()}")
+      // println(s"\tio.out: valid=${c.io.out.valid.peek()}, bits=${c.io.out.bits.peek()}")
+      // c.clock.step(1)
     }
   }
 }
